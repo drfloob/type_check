fd3fc29c09b00fc804e4281b1c97a010ad8f7873
aj heller <aj@drfloob.com>
2013-01-29 21:15:23 -0800

    implement badass generic type checker for most erlang terms
    Maybe this is unnecessary, but a thought experiment I did informed
    otherwise. When implementing a "black box" erlang application, in
    dealing with external input from various sources, of varying
    complexity levels, I think it's important to be able to inform the
    client of exactly what piece(s) of their input is(are) incorrect
    (instead of simply crashing on bad input with no real helpful
    information as to why). This type checker fills that void, though I'm
    unsure of whether such a thing already exists, or in what capacity I'm
    reinventing any standard erlang features. It has so far been very
    quick and easy to create, and is useful, so I'm running with it.

---
+++ src/type_check.erl
@@ -0,0 +1,120 @@
+%%%-------------------------------------------------------------------
+%%% @author aj <AJ Heller <aj@drfloob.com>>
+%%% @copyright (C) 2013, aj
+%%%
+%%% @doc Ensures arguments are of the specified type, used for
+%%% validating unknown input
+%%% @end
+%%%
+%%% Created : 29 Jan 2013 by aj <AJ Heller <aj@drfloob.com>>
+%%%-------------------------------------------------------------------
+-module(type_check).
+-export([validate/2]).
+
+validate(List, Types) 
+  when is_list(List), is_list(Types), length(List) =:= length(Types) ->
+    CheckList = lists:zip(List, Types),
+    case collect_invalid(lists:map(fun validate_one/1, CheckList)) of
+	[] ->
+	    {ok, all_valid};
+	Errors ->
+	    {bad_types, Errors}
+    end;
+
+validate(Item, Spec) ->
+    case validate_one({Item, Spec}) of
+	true -> {ok, all_valid};
+	{invalid, Err} -> {bad_types, [Err]}
+    end.
+	     
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Internal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% binary strings only
+validate_one({Value, string}) when is_binary(Value) ->
+    case heuristic_encoding_bin(Value) of
+	utf8 -> true;
+	_ -> {invalid, {Value, string}}
+    end;
+validate_one({Value, integer}) when is_integer(Value) ->
+    true;
+validate_one({Value, number}) when is_number(Value) ->
+    true;
+validate_one({Value, atom}) when is_atom(Value) ->
+    true;
+validate_one({Value, binary}) when is_binary(Value) ->
+    true;
+
+%% applies a predicate function to the Value, which must return the
+%% atom true. This allows preexisting predicates in your models to be
+%% reused for for input validation.
+validate_one({Value, {eval_true, Fun}}) 
+  when is_function(Fun, 1) ->
+    case Fun(Value) of
+	true -> true;
+	_ -> 
+	    {name, FunName} = erlang:fun_info(Fun, name),
+	    {invalid, {Value, FunName}}
+    end;
+
+%% each value in the List must be of type Type
+validate_one({ValueSet, {each, Type}}) when is_list(ValueSet) ->
+    Types = [Type || _ <- ValueSet],
+    validate(ValueSet, Types);
+
+%% each value in the List must match the *corresponding* type in Type
+%% i.e. validate([[1,hi]], [[number, atom]])
+validate_one({ValueList, TypeList}) 
+  when is_list(ValueList), is_list(TypeList), length(ValueList) =:= length(TypeList) ->
+    validate(ValueList, TypeList);
+
+%% each value in the Tuple must match its *corresponding* type Type
+%% i.e. validate([{1,hi}], [{number, atom}])
+validate_one({ValueTuple, TypeTuple}) 
+  when is_tuple(ValueTuple), is_tuple(TypeTuple), tuple_size(TypeTuple) =:= tuple_size(ValueTuple) ->
+    ValueList = tuple_to_list(ValueTuple),
+    TypeList = tuple_to_list(TypeTuple),
+    validate(ValueList, TypeList);
+
+%% if not above, then invalid
+validate_one(Invalid) ->
+    {invalid, Invalid}.
+
+
+
+
+
+%% modified from: http://www.erlang.org/doc/apps/stdlib/unicode_usage.html
+heuristic_encoding_bin(Bin) when is_binary(Bin) ->
+    case unicode:characters_to_binary(Bin,utf8,utf8) of
+	Bin ->
+	    utf8;
+	_ ->
+	    other
+    end.
+
+
+
+
+collect_invalid(Results) ->
+    collect_invalid(Results, []).
+
+
+collect_invalid([], Acc) ->
+    lists:reverse(Acc);
+collect_invalid([true|Rest], Acc) ->
+    collect_invalid(Rest, Acc);
+collect_invalid([{ok, all_valid}|Rest], Acc) ->
+    %% the good nested case
+    collect_invalid(Rest, Acc);
+collect_invalid([{bad_types, BadTypes}|Rest], Acc) ->
+    %% the bad nested case
+    collect_invalid(Rest, lists:reverse(BadTypes) ++ Acc);
+collect_invalid([{invalid, Err}|Rest], Acc) ->
+    collect_invalid(Rest, [Err|Acc]).
+
+
+
---
+++ test/type_check_tests.erl
@@ -0,0 +1,55 @@
+-module(type_check_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+
+singleItem_test_() ->
+    [
+     ?_assertEqual({ok, all_valid}, type_check:validate(1, integer))
+     , ?_assertEqual({ok, all_valid}, type_check:validate(hi, atom))
+     , ?_assertEqual({ok, all_valid}, type_check:validate(<<>>, binary))
+    ].
+
+numbers_test_() ->
+    [
+     ?_assertEqual({ok, all_valid}, type_check:validate([1], [integer]))
+     , ?_assertEqual({ok, all_valid}, type_check:validate([99999999999999], [integer]))
+
+     , ?_assertEqual({ok, all_valid}, type_check:validate([1.0], [number]))
+     , ?_assertEqual({ok, all_valid}, type_check:validate([99999999999.999], [number]))
+
+     %% error cases
+     , ?_assertEqual({bad_types, [{1.0, integer}]}, type_check:validate([1.0], [integer]))
+     , ?_assertEqual({bad_types, [{99999999999.999, integer}]}, type_check:validate([99999999999.999], [integer]))
+     , ?_assertEqual({bad_types, [{hork, integer}]}, type_check:validate([hork], [integer]))
+    ].
+
+simple_test_() ->
+    [
+     %% good cases
+     ?_assertEqual({ok, all_valid}, type_check:validate([<<"hi">>, 1], [string, number]))
+     , ?_assertEqual({ok, all_valid}, type_check:validate([<<"hi">>, 1, [1,2,3.0]]
+							  , [string, number, {each, number}]))
+     , ?_assertEqual({ok, all_valid}, type_check:validate([<<"hi">>, 1, {one, two, three}]
+							  , [string, number, {atom, atom, atom}]))
+     , ?_assertEqual({ok, all_valid}, type_check:validate([<<"hi">>, 1, {42, <<"the answer">>, [haha]}]
+							  , [string, number, {integer, string, [atom]}]))
+
+     %% unnecessary, but proving the feature works
+     , ?_assertEqual({ok, all_valid}, type_check:validate([<<"hi">>], [{eval_true, fun is_binary/1}]))
+     , ?_assertEqual({ok, all_valid}, type_check:validate([[1,2,3]], [{each, {eval_true, fun is_integer/1}}]))
+     
+     %% bad cases
+     , ?_assertEqual({bad_types, [{arst, number}]}
+		     , type_check:validate([<<"hi">>, 1, [1,2,arst]], [string, number, {each, number}]))
+     , ?_assertMatch({bad_types, [{_, string}]}
+		     , type_check:validate([<<"hi", 999999999>>, 1, [1,2,3.0]], [string, number, {each, number}]))
+     , ?_assertMatch({bad_types, [{<<"you">>, number}]}
+		     , type_check:validate([<<"hi">>, <<"you">>, [1,2,3.0]], [string, number, {each, number}]))
+     , ?_assertMatch({bad_types, [{hork, {each, number}}]}
+		     , type_check:validate([<<"hi">>, 1, hork], [string, number, {each, number}]))
+
+     %% unnecessary, but proving the feature works
+     , ?_assertEqual({ok, all_valid}, type_check:validate([<<"hi">>], [{eval_true, fun is_binary/1}]))
+     , ?_assertEqual({ok, all_valid}, type_check:validate([[1,2,3]], [{each, {eval_true, fun is_integer/1}}]))
+     
+    ].
